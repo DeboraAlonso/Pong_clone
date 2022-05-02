ed9dea9c85f2fa35e3de53a772f476245e0a7233
DeboraAlonso <debo.a18@gmail.com>
2022-05-02 19:12:41 -0300

    Hader "Configuration was added"

---
+++ Config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef CONFIG_HPP
+#define CONFIG_HPP
+
+static const unsigned int WIDTH = 800;
+static const unsigned int HEIGHT = 600;
+static const unsigned int BPP = 32;
+
+
+
+
+#endif // CONFIG_HPP
